9411083933622d0ad5b97d92bd6d0d3cc75c3986
Juan Baltierrez <juanbal2808@gmail.com>
2022-11-29 21:14:37 -0600

    prueba deploy

--- src/enviroments/enviroment.prod.ts
+++ src/enviroments/enviroment.prod.ts
@@ -1,8 +1,4 @@
 export const environment = {
   production: true,
-
-  // ----------------------Localhost----------------------------
-  // baseUrl: "http://127.0.0.1:8080/api/", //localhost
-  // baseURLCB: "http://127.0.0.1:8080/", //localhost Codebar
-  // baseURLSocket: "http://127.0.0.1:8080", // Para hacer la conexion a Socket Local
+  baseUrl: 'https://django-webstore-api.herokuapp.com/',
 };
